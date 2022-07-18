d58bb1fc38ca2ed39e024bc6132a166832bd17e4
GeneDev123 <eugenemreyes123@gmail.com>
2022-07-18 14:08:31 +0800

    Update
    Section 4: Adding Markup part 3

--- wordpress/wp-content/themes/wphierarchy/footer.php
+++ wordpress/wp-content/themes/wphierarchy/footer.php
@@ -1,8 +1,15 @@
-    <?php wp_footer(); ?>
+</div> <!-- #content -->
 
+<footer id="colophon" class="site-footer" role="contentinfor">
 
-    </div> <!-- #content --> 
-</div> <!-- #footer -->
+    <a href="<?php echo esc_url(__('https://wordpress.org/', 'wphierarchy'));?>">
+        <?php printf( esc_html__("proudly powered bt %s", "wphierarchy"), "WordPress"); ?>
+    </a>
+</footer>
+
+</div> <!-- #page -->
+
+<?php wp_footer(); ?>
 
 </body>
 </html>
